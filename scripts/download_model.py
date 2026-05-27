7e090739cddf8cb8c1f28d604fe564dd85949615
Aviral Jain <jainaviral2004@gmail.com>
2026-05-27 15:22:31 +0530

    Add LLaMA 2 7B model to benchmarking system

--- scripts/download_model.py
+++ scripts/download_model.py
@@ -25,6 +25,7 @@ MODEL_CONFIGS = {
     'pythia-800m': {'name': 'EleutherAI/pythia-800m', 'params': '800M', 'size': '3.1GB'},
     'tinyllama-1.1b': {'name': 'TinyLlama/TinyLlama-1.1B-Chat-v1.0', 'params': '1.1B', 'size': '4.3GB'},
     'phi-3-mini': {'name': 'microsoft/Phi-3-mini-4k-instruct', 'params': '3.8B', 'size': '7.8GB'},
+    'llama-2-7b': {'name': 'meta-llama/Llama-2-7b-hf', 'params': '7B', 'size': '13.8GB'},
 }
 
 # Default model to download
